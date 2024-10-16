c57cd70cc20e5c324f4f58b9957998c788b0eb71
safa hamrouni <safa@fanprime.io>
2024-10-16 23:55:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+// Components
+import SearchBar from './components/SearchBar.vue'
+</script>
+
+<template>
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+    <div class="wrapper">
+      <SearchBar msg="Search Bar" />
+    </div>
+  </header>
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,52 @@
+<script setup>
+// Vue
+import { ref } from 'vue'
+
+// Data 
+import data from "../dummyData.json"
+
+// Components
+import Data from "./Data.vue"
+
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+
+const text = ref("")
+</script>
+
+<template>
+  <div class="searchBarWrapper">
+    <h1 class="green">{{ msg }}</h1>
+    <input
+      class="searchBar"
+      :value="text"
+      @input="event => text = event.target.value"
+      placeholder="Type here..."
+    >
+    <div v-for="info in data">
+      <Data :info="info"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+.searchBar {
+  width: 420px;
+  height: 45px;
+  border-radius: 10px;
+  padding: 10px;
+  margin: auto;
+  margin-bottom: 25px;
+}
+</style>
